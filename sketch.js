3087686f173aa2897c9307d13a2e056f6ed9463c
sidhuPaji-777 <82391641+sidhuPaji-777@users.noreply.github.com>
2021-06-12 12:39:13 +0530

    Updated

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ const Constraint = Matter.Constraint;
 var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5;
 var rope1, rope2, rope3, rope4, rope5
 var roof;
+var bobDiameter = 65;
 
 function preload()
 {
@@ -23,21 +24,21 @@ function setup() {
 	//Create the Bodies Here.
 
 	// Creating Bobs
-	bobObject1 = new Bob(width/2-180, height/2+220, 85);
-	bobObject2 = new Bob(width/2-95, height/2+220, 85);
-	bobObject3 = new Bob(width/2-10, height/2+220, 85);
-	bobObject4 = new Bob(width/2+75, height/2+220, 85);
-	bobObject5 = new Bob(width/2+160, height/2+220, 85);
+	bobObject1 = new Bob(width/2-180, height/2+220, bobDiameter);
+	bobObject2 = new Bob(width/2-95, height/2+220, bobDiameter);
+	bobObject3 = new Bob(width/2-10, height/2+220, bobDiameter);
+	bobObject4 = new Bob(width/2+75, height/2+220, bobDiameter);
+	bobObject5 = new Bob(width/2+160, height/2+220, bobDiameter);
 	
 	// Creating Ground
-	roof = new Roof(width/2, height/2-250, 650, 15);
+	roof = new Roof(width/2, height/2-250, 650, 20);
 
 	// Creating rope
-	rope1 = new Chain(bobObject1.body, roof.body, -85*2, 0)
-	rope2 = new Chain(bobObject2.body, roof.body, -45*2, 0)
-	rope3 = new Chain(bobObject3.body, roof.body, -5*2, 0)
-	rope4 = new Chain(bobObject4.body, roof.body, +35*2, 0)
-	rope5 = new Chain(bobObject5.body, roof.body, +75*2, 0)
+	rope1 = new Chain(bobObject1.body, roof.body, -bobDiameter*2, 0)
+	rope2 = new Chain(bobObject2.body, roof.body, -bobDiameter*1, 0)
+	rope3 = new Chain(bobObject3.body, roof.body, -bobDiameter*0, 0)
+	rope4 = new Chain(bobObject4.body, roof.body, -bobDiameter*-1, 0)
+	rope5 = new Chain(bobObject5.body, roof.body, -bobDiameter*-2, 0)
 
 	// Slingshot = new SlingShot(bobObject1.body,{x:200, y:50});
 	Engine.run(engine);
@@ -48,8 +49,9 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+  bobObject1.debug=true;
   
-
+  
   bobObject1.display();
   bobObject2.display();
   bobObject3.display();
@@ -69,7 +71,8 @@ function draw() {
 function keyPressed() {
 	if (keyCode === UP_ARROW)
   {
-	  Matter.Body.applyForce(bobObject1.body, bobObject1.body.position, {x:10, y:-100})
+	  Matter.Body.applyForce(bobObject1.body, bobObject1.body.position,{x:-50*7/3,y:-45*7/3})
   }
 }
 
+// DONE
