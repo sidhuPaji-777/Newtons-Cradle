362095f07877f5ab1649295f13bf7051a3fffeff
sidhuPaji-777 <82391641+sidhuPaji-777@users.noreply.github.com>
2021-06-12 12:40:27 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -4,12 +4,13 @@ class Bob
     {
 
         var option = {
-            isStatic:true,
+            isStatic:false,
             restitution:1,
             friction:0,
             density:0.8
         }
-        this.body = Bodies.circle(x, y, radius, option);
+        this.body = Bodies.circle(x, y, radius/2, option);
+        // this.body = Bodies.circle(x, y, radius, option);
         World.add(world, this.body);
 
         this.radius = radius;
@@ -19,13 +20,19 @@ class Bob
     
     display()
     {
-        fill(rgb(0, 163, 204));
-        stroke("pink");
-        strokeWeight(1.5);
-        var pos = this.body.position;
-        ellipseMode(CENTER);
-        ellipse(pos.x ,pos.y+20, this.radius);
-
+        // fill(rgb(0, 163, 204));
+        // var pos = this.body.position;
+        // ellipseMode(CENTER);
+        // ellipse(pos.x ,pos.y+20, this.radius);
+        var pos=this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        strokeWeight(3);
+        fill(rgb(0, 153, 255));
+        stroke(rgb(1, 137, 228));
+        ellipse(0,35,this.radius, this.radius);
+        pop();
     }
 
 
